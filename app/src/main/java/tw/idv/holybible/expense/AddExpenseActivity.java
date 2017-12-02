1f3d3ecf363a8c82fecf156e29cc69c00a1f6947
CChang <holybible934@gmail.com>
2017-12-02 11:08:00 +0800

    Make Add Expense button work
    Implement insertData() and set OnClickListener in OnCreate

--- app/src/main/java/tw/idv/holybible/expense/AddExpenseActivity.java
+++ app/src/main/java/tw/idv/holybible/expense/AddExpenseActivity.java
@@ -1,9 +1,13 @@
 package tw.idv.holybible.expense;
 
+import android.content.ContentValues;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AlertDialogLayout;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddExpenseActivity extends AppCompatActivity {
@@ -12,6 +16,7 @@ public class AddExpenseActivity extends AppCompatActivity {
     private EditText edDate;
     private EditText edAmount;
     private EditText edExpName;
+    private Button addExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +24,19 @@ public class AddExpenseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
 
         findViews();
-
+        addExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertData(v);
+            }
+        });
     }
 
     private void findViews() {
         edExpName = findViewById(R.id.edExpName);
         edAmount = findViewById(R.id.edAmount);
         edDate = findViewById(R.id.edDate);
+        addExpense = findViewById(R.id.btnAddExp);
     }
 
     public void insertData(View view) {
@@ -34,6 +45,21 @@ public class AddExpenseActivity extends AppCompatActivity {
         int amount = Integer.parseInt(edAmount.getText().toString());
 
         Log.d(TAG, "insertData: " + expName + ", " + amount + ", " + date );
+
+        ExpenseHelper expenseHelper = new ExpenseHelper(this);
+        ContentValues values = new ContentValues();
+        values.put("cdate", date);
+        values.put("expense_name", expName);
+        values.put("amount", amount);
+        long id = expenseHelper.getWritableDatabase().insert(ExpenseContract.EXPENSE_TABLE, null, values);
+        if (id == -1) {
+            new AlertDialog.Builder(this).setMessage("Adding new expense failed")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+        else {
+            finish();
+        }
 
     }
 }
