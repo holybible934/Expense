2320b122d443f4fba0f5b501ec5d76813e913901
CChang <holybible934@gmail.com>
2017-12-09 16:32:11 +0800

    Add UriMatcher handler
    Switch cases to select all Expenses or certain Expense with its id

--- app/src/main/java/tw/idv/holybible/expense/ExpenseProvider.java
+++ app/src/main/java/tw/idv/holybible/expense/ExpenseProvider.java
@@ -1,7 +1,9 @@
 package tw.idv.holybible.expense;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -17,19 +19,47 @@ public class ExpenseProvider extends ContentProvider {
     private ExpenseHelper helper;
     private final String TAG = ExpenseProvider.class.getSimpleName();
 
+    public static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private static final int EXPENSES = 200;
+
+    private static final int EXPENSES_WITH_ID = 100;
+
+    static {
+        sUriMatcher.addURI(ExpenseContract.AUTHORITY, ExpenseContract.EXPENSE_TABLE, EXPENSES);
+        sUriMatcher.addURI(ExpenseContract.AUTHORITY, ExpenseContract.EXPENSE_TABLE + "/#", EXPENSES_WITH_ID);
+    }
+
     @Override
     public boolean onCreate() {
         helper = new ExpenseHelper(getContext());
-        Log.d(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate: Expense Provide has been created.");
         return false;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        Cursor cursor = helper.getReadableDatabase().query(
-                ExpenseContract.EXPENSE_TABLE,
-                projection, selection, selectionArgs, null, null, sortOrder);
+        Cursor cursor = null;
+        switch (sUriMatcher.match(uri)) {
+            case EXPENSES:
+                cursor = helper.getReadableDatabase().query(
+                        ExpenseContract.EXPENSE_TABLE,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case EXPENSES_WITH_ID:
+                long id = ContentUris.parseId(uri);
+                if (selection == null) {
+                    selection = ExpenseContract.COL_ID + "= " + id;
+                }
+                else {
+                    selection += ("AND" + ExpenseContract.COL_ID + "= " + id);
+                }
+
+                cursor = helper.getReadableDatabase().query(
+                        ExpenseContract.EXPENSE_TABLE,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+        }
         return cursor;
     }
 
