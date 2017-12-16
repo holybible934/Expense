ea3c1218814855a39e0b703d83d89017e942660a
CChang <holybible934@gmail.com>
2017-12-16 11:19:08 +0800

    Read initial data from JSON file and insert into SQLite

--- app/src/main/java/tw/idv/holybible/expense/ExpenseHelper.java
+++ app/src/main/java/tw/idv/holybible/expense/ExpenseHelper.java
@@ -1,10 +1,15 @@
 package tw.idv.holybible.expense;
 
+import android.content.ContentValues;
 import android.content.Context;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,21 +37,42 @@ public class ExpenseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(ExpenseContract.CREATE_EXPENSE_TABLE);
+        readJsonData(db);
+
+    }
+
+    private void readJsonData(SQLiteDatabase db) {
         // Read JSON file to initialize database
         InputStream inputStream = context.getResources().openRawResource(R.raw.expenses);
         InputStreamReader inputReader = new InputStreamReader(inputStream);
         BufferedReader buffer = new BufferedReader(inputReader);
+        StringBuilder sb = new StringBuilder();
         try {
             String line = buffer.readLine();
-            StringBuilder sb = new StringBuilder();
             while (line != null) {
                 sb.append(line);
                 line = buffer.readLine();
             }
+            Log.d(TAG, "readJsonData: whole StringBuilder: " + sb.toString());
+            // Parse StringBuilder and insert into Database
+            JSONObject json = new JSONObject(sb.toString());
+            JSONArray array = json.getJSONArray("expenses");
+            ContentValues values = new ContentValues();
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject record = array.getJSONObject(i);
+                values.put("cdate", record.getString("cdate"));
+                values.put("expName", record.getString("expName"));
+                values.put("amount", record.getInt("amount"));
+            }
+            db.insert(ExpenseContract.EXPENSE_TABLE, null,values);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        Log.d(TAG, "onCreate: Create DB and load initial data from res/raw/expenses.json");
+
+
     }
 
     @Override
