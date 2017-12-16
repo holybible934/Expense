306fec72c89e4605d32dea1542ecfbf001b14bb8
CChang <holybible934@gmail.com>
2017-12-16 10:59:49 +0800

    Add InputStream / Reader
    To read all data in res/raw/expenses.json

--- app/src/main/java/tw/idv/holybible/expense/ExpenseHelper.java
+++ app/src/main/java/tw/idv/holybible/expense/ExpenseHelper.java
@@ -4,14 +4,24 @@ import android.content.Context;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by chchang on 2017/11/25.
  */
 
 public class ExpenseHelper extends SQLiteOpenHelper {
+    private Context context;
+    private String TAG = ExpenseHelper.class.getSimpleName();
+
     public ExpenseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+        this.context = context;
     }
 
     public ExpenseHelper(Context context) {
@@ -22,6 +32,21 @@ public class ExpenseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(ExpenseContract.CREATE_EXPENSE_TABLE);
+        // Read JSON file to initialize database
+        InputStream inputStream = context.getResources().openRawResource(R.raw.expenses);
+        InputStreamReader inputReader = new InputStreamReader(inputStream);
+        BufferedReader buffer = new BufferedReader(inputReader);
+        try {
+            String line = buffer.readLine();
+            StringBuilder sb = new StringBuilder();
+            while (line != null) {
+                sb.append(line);
+                line = buffer.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "onCreate: Create DB and load initial data from res/raw/expenses.json");
     }
 
     @Override
