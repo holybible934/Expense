53ebbba8cf055cac0381a19c62b3b4d0675b6650
CHChang <holybible934@gmail.com>
2018-10-28 15:46:32 +0800

    1. Reconstruct layout of onboard activity 2. Add indiactor layout to contain indicators 3. Add horizontal guideline in fragment page1 for the position of button in Page1 fragment

--- app/src/main/java/tw/idv/holybible/expense/OnboardActivity.java
+++ app/src/main/java/tw/idv/holybible/expense/OnboardActivity.java
@@ -1,6 +1,12 @@
 package tw.idv.holybible.expense;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -8,12 +14,17 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+
 public class OnboardActivity extends FragmentActivity {
 
     private static final int ITEMS = 4;
 
     MyAdapter fragAdapter;
     ViewPager mPager;
+    private ArrayList<ImageView> mDots;
+    private int mFragmentCount = 4;
+    private LinearLayout mIndicator;
+    private FrameLayout mIndicatorsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,21 @@ public class OnboardActivity extends FragmentActivity {
         fragAdapter = new MyAdapter(getSupportFragmentManager());
         mPager = findViewById(R.id.pager);
         mPager.setAdapter(fragAdapter);
+
+        mIndicatorsContainer = findViewById(R.id.indicators_container);
+        mIndicator = (LinearLayout) View.inflate(this, R.layout.indicator, null);
+        mIndicatorsContainer.addView(mIndicator);
+        mDots = new ArrayList<>();
+        for (int i = 0; i < mFragmentCount; i++) {
+            ImageView dot = new ImageView(this);
+            dot.setImageDrawable(getDrawable(R.drawable.indicator_dot_grey));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            mIndicator.addView(dot, params);
+            mDots.add(dot);
+        }
     }
 
     public static class MyAdapter extends FragmentPagerAdapter {
@@ -33,6 +59,7 @@ public class OnboardActivity extends FragmentActivity {
 
         @Override
         public Fragment getItem(int position) {
+
             return PageFragment.newInstance(position);
         }
 
@@ -41,4 +68,5 @@ public class OnboardActivity extends FragmentActivity {
             return ITEMS;
         }
     }
+
 }
